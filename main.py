bb252a0a7c6ae68058ff907e4c4787f246bab372
samuel <samuels.g.desouza@gmail.com>
2022-01-24 20:17:10 -0300

    adicionando sintetizador de voz

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
 from vosk import Model, KaldiRecognizer
-import os
 import pyaudio
+import pyttsx3
+import json
 
-# Reconhecimento de fala
 
+# Configurando sintetizador de voz
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[-2].id)
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+# Configurando reconhecimento de fala
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
-
 p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
+
+# Iniciando projeto
 while True:
     data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        resultado = rec.Result()
+        resultado = json.loads(resultado)
 
-print(rec.PartialResult())
+        if resultado is not None:
+            text = resultado["text"]
+            print(text)
+            speak(text)
